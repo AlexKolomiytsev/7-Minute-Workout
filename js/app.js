4c7d045a638c736cb02d3b00ec0b3b275a656a52
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2016-03-18 21:18:30 +0200

    Implemented basic functionality

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sanya on 04.03.2016.
+ */
+'use strict';
+
+/**
+ * create a module named 'app'(1st arg).
+ * 2nd arg is an array of the other module dependencies
+ * that the app module has
+**/
+angular.module('app', []);
+angular.module('7minWorkout', []);
+
