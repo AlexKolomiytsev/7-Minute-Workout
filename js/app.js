e144799396a6b123d306244eb6d109923319a069
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2016-03-19 18:50:59 +0200

    Added decription and video panels

--- js/app.js
+++ js/app.js
@@ -10,13 +10,27 @@
  * that the app module has
  **/
 angular.module('app', ['ngRoute', '7minWorkout']).
-config(['$routeProvider',
-	function ($routeProvider) {
-		$routeProvider.when('/start', { templateUrl: 'partials/start.html' });
-		$routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
-		$routeProvider.when('/finish', { templateUrl: 'partials/finish.html' });
-		$routeProvider.otherwise({ redirectTo: '/start' });
-}]);
+config(function ($routeProvider, $locationProvider, $sceDelegateProvider) {
+		$routeProvider.when('/start', {
+			templateUrl: 'partials/start.html'
+		});
+		$routeProvider.when('/workout', {
+			templateUrl: 'partials/workout.html',
+			controller: 'WorkoutController'
+		});
+		$routeProvider.when('/finish', {
+			templateUrl: 'partials/finish.html'
+		});
+		$routeProvider.otherwise({
+			redirectTo: '/start'
+		});
+		$sceDelegateProvider.resourceUrlWhitelist([
+				'self',
+				'http://*.youtube.com/**'
+		]);
+		//$locationProvider.html5Mode(true);
+	}
+);
 
 angular.module('7minWorkout', []);
 
